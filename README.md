74293cd0edf0177bca6dab5c7fd3b43c8dcbded5
Alan Kebert <adkebert@yahoo.com>
2017-02-26 13:47:49 -0500

    added react
    woohoo, finally got the baseline packages/env set up to try out react on this project

---
+++ README.md
@@ -0,0 +1,4 @@
+This application is intended to help the host of a common bar trivia format
+keep track of teams, scores, and questions.
+
+It runs on Electron with React and is developed with Webpack, Babel, and Yarn.
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
 var path = require('path');
 
-module.exports = {
+const config = {
   entry: './entry.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader?presets[]=es2015',
+      }
+    ]
   }
-}
+};
+
+module.exports = config;
